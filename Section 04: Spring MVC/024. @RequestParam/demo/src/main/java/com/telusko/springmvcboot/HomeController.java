d729cfbb2604ba9e2411c57db9c4e2f95f8dcd6c
A113837 <padi196@live.com>
2020-07-16 11:57:41 +0300

    Add files via upload

---
+++ Section 04: Spring MVC/024. @RequestParam/demo/src/main/java/com/telusko/springmvcboot/HomeController.java
@@ -0,0 +1,28 @@
+package com.telusko.springmvcboot;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/")
+	public String home() {
+		System.out.println("Home page requsted");
+		return "index.jsp";
+	}
+	
+	@RequestMapping("add")
+	public String add(@RequestParam("num1")int i,@RequestParam("num2") int j, HttpServletRequest session)
+	{
+		
+		int num3 = i+j;
+		
+		session.setAttribute("num3", num3);
+		return "result.jsp";
+	}
+}
