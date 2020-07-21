517b6e74159f123728d2fe4ea3324c803d4134f9
A113837 <padi196@live.com>
2020-07-21 14:46:40 +0800

    Add files via upload

---
+++ Section 04: Spring MVC/030. ModelAttribute at Method/src/main/java/com/telusko/springmvcboot/HomeController.java
@@ -0,0 +1,56 @@
+package com.telusko.springmvcboot;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.telusko.springmvcboot.model.Alien;
+
+@Controller
+public class HomeController {
+	
+	@ModelAttribute
+	public void modelData(Model m) {
+		
+		m.addAttribute("name", "Aliens");
+	}
+	
+	@RequestMapping("/")
+	public String home() {
+		System.out.println("Home page requsted");
+		return "index";
+	}
+	
+	@RequestMapping("add")
+	public String add(@RequestParam("num1")int i,@RequestParam("num2") int j, ModelMap m)
+	{
+		//ModelAndView mv = new ModelAndView("result");
+		//mv.setViewName("result");
+		
+		int num3 = i+j;
+		
+		//session.setAttribute("num3", num3);
+		
+		m.addAttribute("num3", num3);
+		return "result";
+	}
+	
+	@RequestMapping("addAlien")
+	public String addAlien(@ModelAttribute("a1") Alien a) {
+		
+//		Alien a = new Alien();
+//		a.setAid(aid);
+//		a.setAname(aname);
+		
+		
+		//m.addAttribute("alien", a);
+		return "result";
+	}
+}
